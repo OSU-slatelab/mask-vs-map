fix bug with scaling map

resnet.py
@@ -106,15 +106,17 @@ class ResNet():
         if 'masking' in output_type:
             self.masking = tf.identity(self.outputs)
             self.outputs = tf.multiply(tf.sigmoid(self.outputs), self.inputs - input_min) + input_min
-            self.outputs = self.scale(inputs, name = 'mask', scale_init = 0.5)
             if 'fidelity' in output_type:
+                self.outputs = self.scale(self.outputs, name = 'mask', scale_init = 0.5)
                 self.fidelity = self.fully_connected(flat, out_shape, name="fc_out")
                 self.outputs += self.scale(self.fidelity, name = 'map', scale_init = 0.5)
+            else:
+                self.outputs = self.scale(self.outputs, name = 'mask')
         elif 'fidelity' in output_type:
             self.fidelity = tf.identity(self.outputs)
             if 'map-as-mask-mimic' in output_type:
-                inputs = tf.multiply(self.maskify(self.outputs), self.inputs - input_min) + input_min
-                self.outputs = self.scale(self.fidelity, 'mask')
+                masked_by_map = tf.multiply(self.maskify(self.outputs), self.inputs - input_min) + input_min
+                self.outputs = self.scale(masked_by_map, 'mask')
 
     def fully_connected(self, inputs, output_shape, name="fc"):
         fc = tf.identity(inputs)

train.py
@@ -39,13 +39,13 @@ parser.add_argument("--lr_decay", type=float, default=0.95)
 # Model
 parser.add_argument("--glayers", type=int, default=2)
 parser.add_argument("--gunits", type=int, default=2048)
-parser.add_argument("--gfilters", type=str, default="128 128 256 256")#type=int, nargs="+", default=[128, 128, 256, 256])
+parser.add_argument("--gfilters", type=int, nargs="+", default=[128, 128, 256, 256])
 parser.add_argument("--tlayers", type=int, default=2)
 parser.add_argument("--tunits", type=int, default=2048)
-parser.add_argument("--tfilters", type=str, default="128 128 256 256") #type=int, nargs="+", default=[128, 128, 256, 256])
+parser.add_argument("--tfilters", type=int, nargs="+", default=[128, 128, 256, 256])
 parser.add_argument("--slayers", type=int, default=2)
 parser.add_argument("--sunits", type=int, default=2048)
-parser.add_argument("--sfilters", type=str, default="128 128 256 256")#type=int, nargs="+", default=[128, 128, 256, 256])
+parser.add_argument("--sfilters", type=int, nargs="+", default=[128, 128, 256, 256])
 parser.add_argument("--dropout", type=float, default=0.3, help="percentage of neurons to drop")
 
 # Data
@@ -88,7 +88,7 @@ def run_training():
                     output_type = a.loss_weight.keys() & ['fidelity', 'masking', 'map-as-mask-mimic'],
                     fc_nodes    = a.gunits,
                     fc_layers   = a.glayers,
-                    filters     = [int(f) for f in a.gfilters.split()],
+                    filters     = a.gfilters,
                     dropout     = a.dropout,
                 )
             generator_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
@@ -105,7 +105,7 @@ def run_training():
                     output_dim = a.senones,
                     fc_nodes   = a.tunits,
                     fc_layers  = a.tlayers,
-                    filters    = [int(f) for f in a.tfilters.split()],
+                    filters    = a.tfilters,
                     dropout    = 0,
                 )
             teacher_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='teacher')
@@ -116,6 +116,8 @@ def run_training():
         if load_student or train_student:
             if load_generator or train_generator:
                 inputs = generator.outputs
+                #noisy_min = tf.reduce_min(noisy_inputs)
+                #inputs = tf.multiply(tf.sigmoid(generator.outputs), noisy_inputs - noisy_min) + noisy_min
             else:
                 inputs = tf.placeholder(tf.float32, [None, a.channels, None, a.input_featdim], name='clean')
 
@@ -125,7 +127,7 @@ def run_training():
                     output_dim = a.senones,
                     fc_nodes   = a.sunits,
                     fc_layers  = a.slayers,
-                    filters    = [int(f) for f in a.sfilters.split()],
+                    filters    = a.sfilters,
                     dropout    = a.dropout,
                 )
             student_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='mimic')